c541c667869ccbd1308c3b5be04c6ab13ede98b4
FanChiMao <qaz5517359@gmail.com>
2021-09-09 20:57:51 +0800

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,66 @@
+import os
+import argparse
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+
+import utils
+from utils.csv_utils import write_csv
+from utils.score_utils import calculate_score_B, calculate_score_A
+from dataset import TestDataset
+from model import MLP
+
+model = MLP(n_inputs=13, hidden_layer1=128, hidden_layer2=256, hidden_layer3=128)
+
+
+def demo():
+    torch.multiprocessing.freeze_support()
+    parser = argparse.ArgumentParser(description='Data Regression')
+
+    parser.add_argument('--train_dir', default='./csv_data/training/independent_mean.csv', type=str)
+    parser.add_argument('--test_dir', default='./csv_data/testing/2021test0831.csv', type=str)
+    parser.add_argument('--result_dir', default='.', type=str)
+    parser.add_argument('--weights', default='./checkpoints/MLP/models/best_score_model.pth', type=str)
+    parser.add_argument('--gpus', default='0', type=str, help='CUDA_VISIBLE_DEVICES')
+
+    args = parser.parse_args()
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+
+    utils.load_checkpoint(model, args.weights)
+    model.cuda()
+    model.eval()
+
+    train_dir = args.train_dir
+    test_dir = args.test_dir
+
+    test_dataset = TestDataset(train_dir, test_dir)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    results = []
+    score = 0
+    max_single_score = 0
+    print('===> Start testing~~')
+    with torch.no_grad():
+        for ii, data_test in enumerate(tqdm(test_loader, ncols=50, leave=True), 0):
+            torch.cuda.ipc_collect()
+            torch.cuda.empty_cache()
+
+            input_ = data_test[0].cuda()
+            file_names = data_test[1]
+
+            predict = model(input_)
+            predict = predict.cpu().numpy()
+
+            for batch in range(len(predict)):
+                results.append([file_names[batch].item(), predict[batch].item()])
+
+
+    write_csv(data=results, csv_path=args.result_dir, save_name='results')
+    print('===> Finish writing csv data!')
+
+
+if __name__ == '__main__':
+    demo()
